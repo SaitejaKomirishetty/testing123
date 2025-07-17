caa35086cc00fefbf9df4dfb82467c83fa26b4b2
SAITEJA KOMIRISHETTY <komirishettysaiteja@gmail.com>
2025-07-17 23:37:24 +0530

    did changes

--- feature1file.js
+++ feature1file.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const feature1file = () => {
-    return <div>this is a feature file for feature1 </div>;
+    return (
+        <div>
+            this is a feature file for feature1 this is feature 1 and change
+            this file
+        </div>
+    );
 };
 
 export default feature1file;
